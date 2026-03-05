893d9e4a8b74b5e6f3956e42b81d022ec6d1196f
Joshua Okeleke <jo.okeleke.yahoo.de@gmail.com>
2026-03-05 15:08:27 +0100

    move poligons instead of camera

--- source/draw3d.c
+++ source/draw3d.c
@@ -13,17 +13,17 @@ NE_Camera* Camara;
 //move the Camera on the Global axis
 void CameraMoveGlobal(NE_Camera *cam, PLAYER player) {
     float tmpTo[3];
-    tmpTo[0] = player.position.x / (1 << 12 - LEVEL_RENDER_SIZE) + fixedToFloat(sinLerp(floatToFixed(player.rotation.y / 45, 12)) / 3 * fixedToFloat(cosLerp(floatToFixed(player.rotation.z / 45, 12)), 12), 12);
-    tmpTo[1] = player.position.z / (1 << 12 - LEVEL_RENDER_SIZE) + fixedToFloat(sinLerp(floatToFixed(player.rotation.z / 45, 12)), 21) * 180;
-    tmpTo[2] = player.position.y / (1 << 12 - LEVEL_RENDER_SIZE) + fixedToFloat(cosLerp(floatToFixed(player.rotation.y / 45, 12)) / 3 * fixedToFloat(cosLerp(floatToFixed(player.rotation.z / 45, 12)), 12), 12);
+    tmpTo[0] = fixedToFloat(sinLerp(floatToFixed(player.rotation.y / 45, 12)) / 3 * fixedToFloat(cosLerp(floatToFixed(player.rotation.z / 45, 12)), 12), 12);
+    tmpTo[1] = fixedToFloat(sinLerp(floatToFixed(player.rotation.z / 45, 12)), 21) * 180;
+    tmpTo[2] = fixedToFloat(cosLerp(floatToFixed(player.rotation.y / 45, 12)) / 3 * fixedToFloat(cosLerp(floatToFixed(player.rotation.z / 45, 12)), 12), 12);
 
     NE_AssertPointer(cam, "NULL pointer");
 
     cam->matrix_is_updated = false;
 
-    cam->from[0] = floatToFixed(player.position.x, LEVEL_RENDER_SIZE);
-    cam->from[1] = floatToFixed(player.position.z, LEVEL_RENDER_SIZE);
-    cam->from[2] = floatToFixed(player.position.y, LEVEL_RENDER_SIZE);
+    cam->from[0] = 0;
+    cam->from[1] = 0;
+    cam->from[2] = 0;
 
     cam->to[0] = floattof32(tmpTo[0]);
     cam->to[1] = floattof32(tmpTo[1]); //up and down
@@ -39,9 +39,9 @@ void CameraMoveGlobal(NE_Camera *cam, PLAYER player) {
     NE_ModelSetRot(w_portalgun_model, 0,
                                         player.rotation.y / DERGEESTO511,
                                         0);
-    NE_ModelSetCoord(w_portalgun_model, localPlayer.position.x / (1 << 12 - LEVEL_RENDER_SIZE) + fixedToFloat(sinLerp(floatToFixed(player.rotation.y / 45 - 50, 12)), 12) / 20,
-                                        localPlayer.position.z / (1 << 12 - LEVEL_RENDER_SIZE) - 0.1 * LEVEL_SIZE,
-                                        localPlayer.position.y / (1 << 12 - LEVEL_RENDER_SIZE) + fixedToFloat(cosLerp(floatToFixed(player.rotation.y / 45 - 50, 12)), 12) / 20);
+    NE_ModelSetCoord(w_portalgun_model, fixedToFloat(sinLerp(floatToFixed(player.rotation.y / 45 - 50, 12)), 12) / 20,
+                                        -0.1 * LEVEL_SIZE,
+                                        fixedToFloat(cosLerp(floatToFixed(player.rotation.y / 45 - 50, 12)), 12) / 20);
     NE_ModelDraw(w_portalgun_model);
 
     if (debugVision) {
@@ -62,15 +62,7 @@ void renderPortals(void){
         if(level.portal[i].portaledPlane.plane == NULL)
             continue;
 
-        Vector2 texCoord0 = { 
-            .x=level.portal[i].portaledPlane.plane->x0,
-            .y=level.portal[i].portaledPlane.plane->y0
-            };
-        Vector2 texCoord1 = { 
-            .x=level.portal[i].portaledPlane.plane->x1,
-            .y=level.portal[i].portaledPlane.plane->y1
-            };
-        RenderQuad(level.portal[i].portaledPlane.plane->vertex1, level.portal[i].portaledPlane.plane->vertex2, level.portal[i].portaledPlane.plane->vertex3, level.portal[i].portaledPlane.plane->vertex4, level.portal[i].portaledPlane.plane->material, texCoord0, texCoord1);
+        RenderQuad(level.portal[i].portaledPlane.plane->vertex1, level.portal[i].portaledPlane.plane->vertex2, level.portal[i].portaledPlane.plane->vertex3, level.portal[i].portaledPlane.plane->vertex4, level.portal[i].portaledPlane.plane->material, level.portal[i].portaledPlane.plane->x0, level.portal[i].portaledPlane.plane->x1, level.portal[i].portaledPlane.plane->y0, level.portal[i].portaledPlane.plane->y1);
         
         // // make a pointer so it dosnt have to be "level.portal[i].portaledPlane.plane" evry time
         // PLANE* plane = level.portal[i].portaledPlane.plane;
@@ -145,30 +137,37 @@ void renderPortals(void){
     }
 }
 
-// Render a quad
-void RenderQuad(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4, NE_Material* material, Vector2 texCoord0, Vector2 texCoord1) {
+void RenderQuadNoformat(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4, NE_Material* material, int texCoordx0, int texCoordx1, int texCoordy0, int texCoordy1) {
 
+        // do not render plane if it would overflow the vertex position
+        if((floatToFixed(abs(vertex1.x - localPlayer.position.x), LEVEL_RENDER_SIZE) > RENDERCLIPDISTANCE) | (floatToFixed(abs(vertex1.y - localPlayer.position.y), LEVEL_RENDER_SIZE) > RENDERCLIPDISTANCE) | (floatToFixed(abs(vertex1.z - localPlayer.position.z), LEVEL_RENDER_SIZE) > RENDERCLIPDISTANCE) |
+            (floatToFixed(abs(vertex4.x - localPlayer.position.x), LEVEL_RENDER_SIZE) > RENDERCLIPDISTANCE) | (floatToFixed(abs(vertex4.y - localPlayer.position.y), LEVEL_RENDER_SIZE) > RENDERCLIPDISTANCE) | (floatToFixed(abs(vertex4.z - localPlayer.position.z), LEVEL_RENDER_SIZE) > RENDERCLIPDISTANCE))
+            return;
         // Render a plane
-        NE_PolyFormat(31, 1, NE_LIGHT_0, NE_CULL_BACK, 0);
-
         NE_MaterialUse(material);
 
         NE_PolyBegin(GL_QUAD);
         //NE_PolyNormal(level.Plane[i].nx / 100, level.Plane[i].nz / 100, level.Plane[i].ny / 100);
 
-        NE_PolyTexCoord(texCoord0.y, texCoord1.x);
-        NE_PolyVertexI(floatToFixed(vertex1.x, LEVEL_RENDER_SIZE), floatToFixed(vertex1.z, LEVEL_RENDER_SIZE), floatToFixed(vertex1.y, LEVEL_RENDER_SIZE));
+        NE_PolyTexCoord(texCoordy0, texCoordx1);
+        NE_PolyVertexI(floatToFixed(vertex1.x - localPlayer.position.x, LEVEL_RENDER_SIZE), floatToFixed(vertex1.z - localPlayer.position.z, LEVEL_RENDER_SIZE), floatToFixed(vertex1.y - localPlayer.position.y, LEVEL_RENDER_SIZE));
 
-        NE_PolyTexCoord(texCoord1.y, texCoord1.x);
-        NE_PolyVertexI(floatToFixed(vertex2.x, LEVEL_RENDER_SIZE), floatToFixed(vertex2.z, LEVEL_RENDER_SIZE), floatToFixed(vertex2.y, LEVEL_RENDER_SIZE));
+        NE_PolyTexCoord(texCoordy1, texCoordx1);
+        NE_PolyVertexI(floatToFixed(vertex2.x - localPlayer.position.x, LEVEL_RENDER_SIZE), floatToFixed(vertex2.z - localPlayer.position.z, LEVEL_RENDER_SIZE), floatToFixed(vertex2.y - localPlayer.position.y, LEVEL_RENDER_SIZE));
 
-        NE_PolyTexCoord(texCoord1.y, texCoord0.x);
-        NE_PolyVertexI(floatToFixed(vertex3.x, LEVEL_RENDER_SIZE), floatToFixed(vertex3.z, LEVEL_RENDER_SIZE), floatToFixed(vertex3.y, LEVEL_RENDER_SIZE));
+        NE_PolyTexCoord(texCoordy1, texCoordx0);
+        NE_PolyVertexI(floatToFixed(vertex3.x - localPlayer.position.x, LEVEL_RENDER_SIZE), floatToFixed(vertex3.z - localPlayer.position.z, LEVEL_RENDER_SIZE), floatToFixed(vertex3.y - localPlayer.position.y, LEVEL_RENDER_SIZE));
 
-        NE_PolyTexCoord(texCoord0.y, texCoord0.x);
-        NE_PolyVertexI(floatToFixed(vertex4.x, LEVEL_RENDER_SIZE), floatToFixed(vertex4.z, LEVEL_RENDER_SIZE), floatToFixed(vertex4.y, LEVEL_RENDER_SIZE));
+        NE_PolyTexCoord(texCoordy0, texCoordx0);
+        NE_PolyVertexI(floatToFixed(vertex4.x - localPlayer.position.x, LEVEL_RENDER_SIZE), floatToFixed(vertex4.z - localPlayer.position.z, LEVEL_RENDER_SIZE), floatToFixed(vertex4.y - localPlayer.position.y, LEVEL_RENDER_SIZE));
 
         NE_PolyEnd();
+}
+
+// Render a quad
+void RenderQuad(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4, NE_Material* material, int texCoordx0, int texCoordx1, int texCoordy0, int texCoordy1) {
+    NE_PolyFormat(31, 1, NE_LIGHT_0, NE_CULL_BACK, 0);
+    RenderQuadNoformat(vertex1, vertex2, vertex3, vertex4, material, texCoordx0, texCoordx1, texCoordy0, texCoordy1);
 }
 
 //Render all Planes in the level
@@ -180,27 +179,7 @@ void RenderPlanes(Level level) {
         if(!level.Plane[i].isDrawn)
             continue;
 
-        // Render a plane
-        NE_PolyFormat(31, 1, NE_LIGHT_0, NE_CULL_BACK, 0);
-
-        NE_MaterialUse(level.Plane[i].material);
-
-        NE_PolyBegin(GL_QUAD);
-        //NE_PolyNormal(level.Plane[i].nx / 100, level.Plane[i].nz / 100, level.Plane[i].ny / 100);
-
-        NE_PolyTexCoord(level.Plane[i].y0, level.Plane[i].x1);
-        NE_PolyVertexI(floatToFixed(level.Plane[i].vertex1.x, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex1.z, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex1.y, LEVEL_RENDER_SIZE));
-
-        NE_PolyTexCoord(level.Plane[i].y1, level.Plane[i].x1);
-        NE_PolyVertexI(floatToFixed(level.Plane[i].vertex2.x, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex2.z, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex2.y, LEVEL_RENDER_SIZE));
-
-        NE_PolyTexCoord(level.Plane[i].y1, level.Plane[i].x0);
-        NE_PolyVertexI(floatToFixed(level.Plane[i].vertex3.x, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex3.z, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex3.y, LEVEL_RENDER_SIZE));
-
-        NE_PolyTexCoord(level.Plane[i].y0, level.Plane[i].x0);
-        NE_PolyVertexI(floatToFixed(level.Plane[i].vertex4.x, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex4.z, LEVEL_RENDER_SIZE), floatToFixed(level.Plane[i].vertex4.y, LEVEL_RENDER_SIZE));
-
-        NE_PolyEnd();
+        RenderQuad(level.Plane[i].vertex1, level.Plane[i].vertex2, level.Plane[i].vertex3, level.Plane[i].vertex4,level.Plane[i].material, level.Plane[i].x0, level.Plane[i].x1, level.Plane[i].y0, level.Plane[i].y1);
     }
 }
 
@@ -212,23 +191,18 @@ void RenderDebug(void)
         localPlayer.position.x, localPlayer.position.y, localPlayer.position.z, localPlayer.rotation.x, localPlayer.rotation.y, localPlayer.rotation.z, localPlayer.lookVector.x, localPlayer.lookVector.y, localPlayer.lookVector.z);
     }
     if (debugVision) {
-        NE_PolyFormat(31, 1, NE_LIGHT_0, NE_CULL_NONE, 0);
+        NE_PolyFormat(0, 1, NE_LIGHT_0, NE_CULL_NONE, 0);
         for (int i = 0; i < level.currentHitbox; i++) {
             NE_PolyBegin(GL_QUAD);
             NE_MaterialUse(debugempty);
-            // top and bottom
-            for (int j = 0; j < 8; j++) {
-                NE_PolyVertexI(floatToFixed(level.allHitboxes[i].vertex[j].x, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j].z, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j].y, LEVEL_RENDER_SIZE));
-            }
-            // sides
-            for (int j = 0; j < 8; j += 6) {
-                NE_PolyVertexI(floatToFixed(level.allHitboxes[i].vertex[j].x, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j].z, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j].y, LEVEL_RENDER_SIZE));
-                NE_PolyVertexI(floatToFixed(level.allHitboxes[i].vertex[j + 1].x, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j + 1].z, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j + 1].y, LEVEL_RENDER_SIZE));
-            }
-            for (int j = 2; j < 6; j += 2) {
-                NE_PolyVertexI(floatToFixed(level.allHitboxes[i].vertex[j].x, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j].z, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j].y, LEVEL_RENDER_SIZE));
-                NE_PolyVertexI(floatToFixed(level.allHitboxes[i].vertex[j + 1].x, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j + 1].z, LEVEL_RENDER_SIZE), floatToFixed(level.allHitboxes[i].vertex[j + 1].y, LEVEL_RENDER_SIZE));
-            }
+            
+            RenderQuadNoformat(level.allHitboxes[i].vertex[0], level.allHitboxes[i].vertex[1], level.allHitboxes[i].vertex[2], level.allHitboxes[i].vertex[3], debugempty, 0, 0, 0 ,0);
+            RenderQuadNoformat(level.allHitboxes[i].vertex[4], level.allHitboxes[i].vertex[5], level.allHitboxes[i].vertex[6], level.allHitboxes[i].vertex[7], debugempty, 0, 0, 0 ,0);
+        
+            RenderQuadNoformat(level.allHitboxes[i].vertex[0], level.allHitboxes[i].vertex[1], level.allHitboxes[i].vertex[6], level.allHitboxes[i].vertex[7], debugempty, 0, 0, 0 ,0);
+        
+            RenderQuadNoformat(level.allHitboxes[i].vertex[2], level.allHitboxes[i].vertex[3], level.allHitboxes[i].vertex[4], level.allHitboxes[i].vertex[5], debugempty, 0, 0, 0 ,0);
+
         }
     }
 }
--- source/draw3d.h
+++ source/draw3d.h
@@ -2,11 +2,12 @@
 #define DRAW3D_H
 
 #define DERGEESTO511 0.704500978473582
+#define RENDERCLIPDISTANCE 32768 // 2^16 // todo slightly too high
 
 extern NE_Camera* Camara;
 void Draw3DScene(void);
 
 // Render a quad
-void RenderQuad(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4, NE_Material* material, Vector2 texCoord0, Vector2 texCoord1);
+void RenderQuad(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4, NE_Material* material, int texCoordx0, int texCoordx1, int texCoordy0, int texCoordy1);
 
 #endif
